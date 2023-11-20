2b3a8ce5ec31688ad0e97bb45d7e508df77db78d
Satyam Kumar <786satyamrj@gmail.comgit config --global user.name Satyam git config --global user.email 786satyamrj@gmail.com>
2023-11-20 20:17:53 +0530

    without node modules

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Start your web server (replace the command with the actual command to start your server)
 
-
-
-
-# Start Celery worker
-echo "Starting Celery worker..."
-celery -A main beat --loglevel=info &
-
-# Start Celery beat
-echo "Starting Celery beat..."
-celery -A tasks worker --loglevel=info&
-
+redis-server &
+python app.py &
+python image.py &
+cd v2-frontend ;npm run serve &
